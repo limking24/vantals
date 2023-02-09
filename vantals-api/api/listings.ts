a2b70125a3cc78730336580ec88edf9a3dc56196
kinglim3 <limkingfai@gmail.com>
2023-02-08 22:51:46 -0800

    Fix bugs

--- vantals-api/api/listings.ts
+++ vantals-api/api/listings.ts
@@ -9,7 +9,7 @@ import { CraigslistApi } from '../listing-collector/craigslist-api';
 export default allowCors(async function handler(req, res) {
 	const { minPrice, maxPrice } = req.query;
 	const filter: FilterOption = { minPrice, maxPrice };
-	let collectors: ListingCollector[] = [new CraigslistApi(),  new VanpeopleApi(), new VanskyCrawler()];
+	let collectors: ListingCollector[] = [new CraigslistApi(), new VanpeopleApi(), new VanskyCrawler()];
 	let onFetch: OnMultiFetch = collectors.map(collector => collector.collect(filter));
 	let json = flatten(await Promise.all(onFetch))
 				.sort((a, b) => b.time.getTime() - a.time.getTime());
--- vantals-api/listing-collector/craigslist-api.ts
+++ vantals-api/listing-collector/craigslist-api.ts
@@ -4,7 +4,6 @@ import { ListingCollector } from './listing-collector';
 interface Response {
 	data: {
 		decode: {
-			locations: [number, [number, string, string][]];
 			minPostedDate: number;
 			minPostingId: number;
 		}
@@ -19,26 +18,39 @@ export class CraigslistApi implements ListingCollector {
 		let json = await response.json() as Response;
 		let minPostingId = json.data.decode.minPostingId;
 		return json.data.items.map((item, i) => {
-			let photos;
-			switch (item.length) {
-				case 8: photos = -1; break;
-				case 9: photos = 5; break;
-				case 10: photos = 6; break;
+			let id = minPostingId + item[0];
+			let timestamp = (json.data.decode.minPostedDate + item[1]) * 1000;
+			let price = `${item[3]}`;
+			let title: string;
+			let photo: string | undefined;
+			let bedroom: number | undefined;
+			let sqft: number | undefined;
+			let path: string;
+
+			for (let x = 5; x < item.length; x++) {
+				if (typeof item[x] == 'string') {
+					title = item[x];
+				} else if (Array.isArray(item[x])) {
+					let identifier = item[x][0];
+					if (identifier == 4) {
+						photo = item[x][1].substring(2);
+					} else if (identifier == 5) {
+						bedroom = item[x][1];
+						sqft = item[x][2];
+					} else if (identifier == 6) {
+						path = item[x][1];
+					}
+				}
 			}
-			let hrefInfo = (photos == -1) ? 5 : photos + 1;
-			let title = hrefInfo + 1;
-			let houseInfo = title + 1;
-			let location = json.data.decode.locations[item[hrefInfo][0]][2];
-			let bedroom = item[houseInfo][1];
-			let sqft = item[houseInfo][2];
+
 			return {
-				title: item[title],
-				photo: new URL(photos > 0 ? `https://images.craigslist.org/${item[photos][1].substring(2)}_600x450.jpg` : 'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg'),
-				hyperlink: new URL(`https://vancouver.craigslist.org/${location}/apa/d/${item[hrefInfo][1]}/${minPostingId + item[0]}.html`),
-				description: `${bedroom == 0 ? '' : bedroom + 'br'} ${sqft == 0 ? '' : sqft + 'sf'}`,
-				location,
-				time: new Date((json.data.decode.minPostedDate + item[1]) * 1000),
-				price: `${item[3]}`,
+				title: title!,
+				photo: new URL(photo != undefined ? `https://images.craigslist.org/${photo}_600x450.jpg` : 'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg'),
+				hyperlink: new URL(`https://vancouver.craigslist.org/van/apa/d/${path!}/${id}.html`),
+				description: `${bedroom == undefined ? '' : bedroom + 'br'} ${sqft == undefined ? '' : sqft + 'sf'}`,
+				location: '',
+				time: new Date(timestamp),
+				price,
 				source: 'craigslist'
 			}
 		});
